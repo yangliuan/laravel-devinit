f1621f6d632e95d7cb9ef24cfed65bdd34a44d60
yangliuan <yangliuancn@foxmail.com>
2021-03-05 15:03:25 +0800

    修改

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ class CreateUsersTable extends Migration
         {
             $table->id();
             $table->string('mobile', 15)->default('')->comment('手机号');
-            $table->string('name')->default('')->comment('昵称');
+            $table->string('name', 20)->default('')->comment('昵称');
+            $table->string('avatar', 255)->default('')->comment('头像');
             $table->string('email')->default('')->comment('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->default('')->comment('密码');
