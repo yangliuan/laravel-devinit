f39eea509383e2862438d38687973c036ea755ab
yangliuan <yangliuancn@foxmail.com>
2021-06-24 00:13:45 +0800

    修改

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -3,6 +3,7 @@
 namespace Yangliuan\LaravelDevinit\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Yangliuan\LaravelDevinit\Traits\Register;
 
@@ -41,7 +42,7 @@ class InstallCommand extends Command
         );
 
         //发布公共文件
-        system('php artisan dev:publish --force');
+        $this->call('dev:publish', ['--force' => '--force']);
         //初始化app配置
         $this->regAppConfig();
         //初始化cors配置
@@ -55,17 +56,13 @@ class InstallCommand extends Command
         {
             $this->info('start install laravel/passport...');
             system('composer require laravel/passport');
-            system('php artisan migrate');
-            system('php artisan passport:install --uuids');
-            $this->info('install laravel/passport successed!');
+            $this->call('migrate');
+            system('php artisan passport:install --uuids --force');
             $this->regAuthConfigPassport();
             $this->regAuthServiceProviderByPassort();
             $this->regHttpKernelByPassport();
-
-            $this->call('vendor:publish', [
-                '--tag' => 'devinit-passport',
-                '--force' => 'force',
-            ]);
+            $this->call('vendor:publish', ['--tag' => 'devinit-passport', '--force' => 'force']);
+            $this->info('install laravel/passport successed!');
         }
 
         $loginMethod = $this->choice('please choice users login method', ['mobile-smscode', 'wechat-miniprogram', 'custom'], 0);
@@ -83,20 +80,14 @@ class InstallCommand extends Command
                 //注册短信日志配置
                 $this->regConfigLoggingBySms();
                 //发布短信相关文件
-                $this->call('vendor:publish', [
-                    '--tag' => 'devinit-sms',
-                    '--force' => 'force',
-                ]);
+                $this->call('vendor:publish', ['--tag' => 'devinit-sms', '--force' => 'force']);
             }
         }
         elseif ($loginMethod == 'wechat-miniprogram')
         {
             system('composer require overtrue/wechat:~4.0');
             //发布微信相关文件
-            $this->call('vendor:publish', [
-                '--tag' => 'devinit-wechat',
-                '--force' => 'force',
-            ]);
+            $this->call('vendor:publish', ['--tag' => 'devinit-wechat', '--force' => 'force']);
         }
 
         $this->info('start install overtrue/laravel-lang');
@@ -107,9 +98,8 @@ class InstallCommand extends Command
         $this->info('start install tucker-eric/eloquentfilter');
         system('composer require tucker-eric/eloquentfilter');
         system('php artisan vendor:publish --provider="EloquentFilter\ServiceProvider"');
-        $this->info('install tucker-eric/eloquentfilter successed!');
-
         $this->regEloquentfilterConfig();
+        $this->info('install tucker-eric/eloquentfilter successed!');
 
         if ($this->choice('Do you want to install composer require yangliuan/generator?', ['yes', 'no'], 0) === 'yes')
         {
@@ -120,7 +110,7 @@ class InstallCommand extends Command
         {
             system('composer require laravel/horizon');
             system('php artisan horizon:install');
-            system('php artisan migrate');
+            $this->call('migrate');
         }
 
         if ($this->choice('Do you want to install laravel/telescope?', ['yes', 'no'], 0) === 'yes')
@@ -128,7 +118,7 @@ class InstallCommand extends Command
             $this->info('start install laravel/telescope...');
             system('composer require laravel/telescope');
             system('php artisan telescope:install');
-            system('php artisan migrate');
+            $this->call('migrate');
             $this->info('install laravel/telescope successed!');
         }
 
@@ -141,12 +131,7 @@ class InstallCommand extends Command
             $this->info('install barryvdh/laravel-ide-helper successed!');
         }
 
-
-        $this->call('vendor:publish', [
-            '--tag' => 'devinit-providers',
-            '--force' => 'force',
-        ]);
-
+        $this->call('vendor:publish', ['--tag' => 'devinit-providers', '--force' => 'force']);
         system('php artisan dev:reset');
     }
 }
